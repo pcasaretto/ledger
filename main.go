6153d9ee752fc0fb523b4ef9af285b05a0d428f9
Geoffrey Ragot <geoffrey.ragot@gmail.com>
2022-09-12 12:53:24 +0200

    feat: generate go client code inside the repository

--- main.go
+++ main.go
@@ -1,3 +1,4 @@
+//go:generate docker run --rm -w /local -v ${PWD}:/local openapitools/openapi-generator-cli:latest generate  -i ./pkg/api/controllers/swagger.yaml -g go -o ./client --git-user-id=numary --git-repo-id=ledger -p packageVersion=latest -p isGoSubmodule=true -p packageName=client -t ./sdk/templates/go
 package main
 
 import (
